b3342ef743329fa40b8bc52d2fb26f38eff30255
Kelly Shew <kellyshew@MacBook-Air.local>
2020-07-18 13:19:24 -0700

    peeks east correctly. needs to implement peek(direction) and check if out of range

--- find_words.js
+++ find_words.js
@@ -22,7 +22,49 @@
  * @param {Set} dictionary Contains all words to look for in the letter grid
  * @returns {Set} All words from the dictionary that were found
  */
+
+let myGrid = [ ["P", "R", "S"], ["C", "A", "T"], ["E", "N", "T"], ["D", "O", "G"] ]
+let dictionary = new Set()
+
+dictionary.add("CAT")
+dictionary.add("DOG")
+
 async function findWords(wordGrid, dictionary) {
 	// TODO: Implement me
+
+	let foundWords = new Set();
+
+	for (let row = 0; row < wordGrid.length; row++) {
+		for (let column = 0; column < wordGrid[row].length; column++) {
+
+			let currentLetter = wordGrid[row][column];
+			let currentMatches = [];
+
+			dictionary.forEach( word => {
+				if (word[0] == currentLetter) {
+					 currentMatches.push(word)
+				}
+			})
+			while (currentMatches.length > 0) {
+
+				currentMatch = currentMatches.pop()
+
+				//PEEK EAST
+				for (let i = 0; i < currentMatch.length; i++) {
+					// need to check indices
+					console.log(currentMatch[i] + " and " + wordGrid[row][column + i]);
+					if (currentMatch[i] != wordGrid[row][column + i]) break;
+					if (i == currentMatch.length - 1) {
+						foundWords.add(currentMatch)
+					}
+				}
+
+			}
+		}
+	}
+	console.log(foundWords);
+
 }
+
+findWords(myGrid, dictionary);
 module.exports.findWords = findWords;
