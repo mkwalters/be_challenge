64aa116ad572f57d3f5a6d42c38589641d848bf1
Kelly Shew <kellyshew@MacBook-Air.local>
2020-07-19 12:24:43 -0700

    spacing fix

--- correct.js
+++ correct.js
@@ -2,11 +2,9 @@
  * Put the correct implementation of incorrect.js here.
  */
 
-//spacing
-
 async function pullFirstUser(userIds) {
   for (let i = 0; i < userIds.length; i++) {
-        let user = await models.user.findOne({_id: userIds[i]});
+    let user = await models.user.findOne({_id: userIds[i]});
     if (user) return user;
   };
 }
@@ -18,7 +16,7 @@ async function pullPaymentsForUsers(users) {
   for (let i = 0; i < users.length; i++) {
     let payments = await models.payment.find({user: users[i]._id})
     result.push(payments);
-    }
+  }
   return result; // array of array with payments (the first array should contain payments for the first user)
 }
 module.exports.pullPaymentsForUsers = pullPaymentsForUsers;
@@ -37,17 +35,17 @@ async function getPaymentWithUser(paymentId) {
       payment[0].user = user[0]
     }
   }
-    return payment;
+  return payment;
 }
 module.exports.getPaymentWithUser = getPaymentWithUser;
 
 async function getGroupedUserPmts(userIds) {
   let result = {};
-    let payments = await models.payment.find({active: true});
+  let payments = await models.payment.find({active: true});
   userIds.forEach(userId => {
     usersPayments = payments.filter(payment => userId == payment.user);
     result[userId] = usersPayments;
-    });
+  });
   return result;
 }
 module.exports.getGroupedUserPmts = getGroupedUserPmts;
